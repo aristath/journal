519c55f4a13f05f0a2dd313fbc70a0862619fdbc
Aristeides Stathopoulos <aristath@gmail.com>
2016-03-06 11:36:09 +0200

    import files from _s

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Journal Theme Customizer.
+ *
+ * @package Journal
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function journal_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'journal_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function journal_customize_preview_js() {
+	wp_enqueue_script( 'journal_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'journal_customize_preview_js' );
